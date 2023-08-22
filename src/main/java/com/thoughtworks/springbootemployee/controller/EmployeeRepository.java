8eb35585223b27cf503a64f19c2c8de166995211
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-22 16:10:43 +0800

    feat: add Employee and listAll class mapping api

---
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeRepository.java
@@ -0,0 +1,23 @@
+package com.thoughtworks.springbootemployee.controller;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+@Repository
+public class EmployeeRepository {
+    private static final List<Employee> employees = new ArrayList<>();
+
+    static {
+        employees.add(new Employee(1L, "Alice", 30, "Female", 500));
+        employees.add(new Employee(2L, "Bob", 23, "Male", 699));
+        employees.add(new Employee(3L, "Sandra", 66, "Female", 788));
+        employees.add(new Employee(4L, "Sam", 34, "Male", 4566));
+        employees.add(new Employee(5L, "Aubrey", 69, "Male", 6969));
+
+    }
+
+    public List<Employee> listAll() {
+        return employees;
+    }
+}
--- src/main/java/com/thoughtworks/springbootemployee/controller/HelloController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/HelloController.java
@@ -1,16 +1,28 @@
 package com.thoughtworks.springbootemployee.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/employees")
 public class HelloController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
     @GetMapping(path = "/{userName}")
     public String getAll(@PathVariable String userName) {
 
         return "Hello:" + userName;
+    }
+
+    @GetMapping
+    public List<Employee> listAll() {
+        return employeeRepository.listAll();
     }
 }
