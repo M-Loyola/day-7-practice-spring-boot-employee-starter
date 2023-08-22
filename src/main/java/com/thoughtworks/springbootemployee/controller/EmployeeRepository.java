6313770f8f274704aaad71ddfc756239d6454370
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-22 20:03:28 +0800

    refactor: reformat lines and remove unused imports

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeRepository.java
@@ -1,11 +1,6 @@
 package com.thoughtworks.springbootemployee.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,11 +11,11 @@ public class EmployeeRepository {
     private static final List<Employee> employees = new ArrayList<>();
 
     static {
-        employees.add(new Employee(1L, "Alice", 30, "Female", 500,1));
-        employees.add(new Employee(2L, "Bob", 23, "Male", 699,2));
-        employees.add(new Employee(3L, "Sandra", 66, "Female", 788,3));
-        employees.add(new Employee(4L, "Sam", 34, "Male", 4566,5));
-        employees.add(new Employee(5L, "Aubrey", 21, "Male", 6900,1));
+        employees.add(new Employee(1L, "Alice", 30, "Female", 500, 1));
+        employees.add(new Employee(2L, "Bob", 23, "Male", 699, 2));
+        employees.add(new Employee(3L, "Sandra", 66, "Female", 788, 3));
+        employees.add(new Employee(4L, "Sam", 34, "Male", 4566, 5));
+        employees.add(new Employee(5L, "Aubrey", 21, "Male", 6900, 1));
 
     }
 
@@ -41,29 +36,31 @@ public class EmployeeRepository {
                 .collect(Collectors.toList());
     }
 
-    public Employee save(Employee employee){
+    public Employee save(Employee employee) {
         Long id = generateNextId();
-        Employee toBeSavedEmployee = new Employee(id, employee.getName(),employee.getAge(),employee.getGender(),employee.getSalary(), employee.getCompanyId());
+        Employee toBeSavedEmployee = new Employee(id, employee.getName(), employee.getAge(), employee.getGender(), employee.getSalary(), employee.getCompanyId());
         employees.add(toBeSavedEmployee);
         return toBeSavedEmployee;
     }
 
-    public Long generateNextId(){
+    public Long generateNextId() {
         return employees.stream()
                 .mapToLong(Employee::getId)
                 .max()
                 .orElse(0L) + 1L;
     }
+
     public void delete(Employee employee) {
         employees.remove(employee);
     }
 
     public List<Employee> listByPage(Long pageNumber, Long pageSize) {
         return employees.stream()
-                .skip((pageNumber-1)*pageSize)
+                .skip((pageNumber - 1) * pageSize)
                 .limit(pageSize)
                 .collect(Collectors.toList());
     }
+
     public List<Employee> findByCompanyId(Long companyId) {
         return employees.stream()
                 .filter(employee -> employee.getCompanyId() == companyId)
