cbeab72bf8b21c966d4b8c081affecc5ffc1f3c1
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-23 13:57:20 +0800

    test: add  test for  should_return_the_employees_by_given_gender_when_perform_get_employee(

--- src/test/java/com/thoughtworks/springbootemployee/EmployeeApiTests.java
+++ src/test/java/com/thoughtworks/springbootemployee/EmployeeApiTests.java
@@ -65,4 +65,20 @@ class EmployeeApiTests {
         mockMvcClient.perform(MockMvcRequestBuilders.get("/employees/" + notExistEmployeeId))
                 .andExpect(status().isNotFound());
     }
+    @Test
+    void should_return_the_employees_by_given_gender_when_perform_get_employee() throws Exception {
+        //Given
+        Employee Sam =  employeeRepository.insert(new Employee(null,"Sam",23,"Male",1200, 1L));
+        employeeRepository.insert(new Employee(null,"Luna",23,"Female",1200, 1L));
+        //When Then
+        mockMvcClient.perform(MockMvcRequestBuilders.get("/employees").param("gender","Male"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$",hasSize(1)))
+                .andExpect(jsonPath("$[0].id").value(Sam.getId()))
+                .andExpect(jsonPath("$[0].name").value(Sam.getName()))
+                .andExpect(jsonPath("$[0].age").value(Sam.getAge()))
+                .andExpect(jsonPath("$[0].gender").value(Sam.getGender()))
+                .andExpect(jsonPath("$[0].salary").value(Sam.getSalary()))
+                .andExpect(jsonPath("$[0].companyId").value(Sam.getCompanyId()));
+    }
 }
