dc45de47fe1b3877ccef7f88e6e0caec0e3d51aa
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-23 15:47:50 +0800

    feat: add handling and test for should_return_created_employee_when_create_given_employee_service_and_emplooyee_with_valid_age

---
+++ src/test/java/com/thoughtworks/springbootemployee/service/EmployeeServiceTest.java
@@ -0,0 +1,37 @@
+package com.thoughtworks.springbootemployee.service;
+
+import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.stubbing.Answer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class EmployeeServiceTest {
+    private EmployeeService employeeService;
+    private EmployeeRepository mockedEmployeeRepository;
+    @BeforeEach
+    void setUp(){
+        mockedEmployeeRepository = mock(EmployeeRepository.class);
+        employeeService = new EmployeeService(mockedEmployeeRepository);
+    }
+    @Test
+    void should_return_created_employee_when_create_given_employee_service_and_emplooyee_with_valid_age(){
+        //Given
+        Employee employee = new Employee(null, "Sam", 23, "Female", 1200, 1L);
+        Employee savedEmployee = employee;
+        when(mockedEmployeeRepository.insert(employee)).thenReturn(savedEmployee);
+        //When
+        Employee employeeResponse = employeeService.create(employee);
+        //Then
+        assertEquals(savedEmployee.getId(),employeeResponse.getId());
+        assertEquals("Sam",employeeResponse.getName());
+        assertEquals(23,employeeResponse.getAge());
+        assertEquals("Female",employeeResponse.getGender());
+        assertEquals(1200,employeeResponse.getSalary());
+    }
+}
+
