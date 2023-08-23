957ceec4094999afb9c15ca1bc8f3133de87f0a7
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-23 14:48:14 +0800

    test: add test for should_return_page_query_when_perform_list_employee_by_page_given_page_number_and_page_size

--- src/test/java/com/thoughtworks/springbootemployee/EmployeeApiTests.java
+++ src/test/java/com/thoughtworks/springbootemployee/EmployeeApiTests.java
@@ -138,4 +138,32 @@ class EmployeeApiTests {
         mockMvcClient.perform(MockMvcRequestBuilders.delete("/employees/" + Sam.getId()))
                 .andExpect(status().isNoContent());
     }
+    @Test
+    void should_return_page_query_when_perform_list_employee_by_page_given_page_number_and_page_size() throws Exception {
+        //Given
+        Long pageNumber = 1L;
+        Long pageSize = 2L;
+        Employee Sam = employeeRepository.insert(new Employee(null, "Sam", 23, "Female", 1200, 1L));
+        Employee Luna = employeeRepository.insert(new Employee(null, "Luna", 23, "Female", 1200, 1L));
+        Employee Richard = employeeRepository.insert(new Employee(null, "Richard", 23, "Male", 1200, 1L));
+        Employee Albert = employeeRepository.insert(new Employee(null, "Albert", 23, "Male", 1200, 1L));
+        //When Then
+        mockMvcClient.perform(MockMvcRequestBuilders.get("/employees")
+                        .param("pageNumber", String.valueOf(pageNumber))
+                        .param("pageSize",String.valueOf(pageSize)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(Math.toIntExact(pageSize))))
+                .andExpect(jsonPath("$[0].id").value(Sam.getId()))
+                .andExpect(jsonPath("$[0].name").value(Sam.getName()))
+                .andExpect(jsonPath("$[0].age").value(Sam.getAge()))
+                .andExpect(jsonPath("$[0].gender").value(Sam.getGender()))
+                .andExpect(jsonPath("$[0].salary").value(Sam.getSalary()))
+                .andExpect(jsonPath("$[0].companyId").value(Sam.getCompanyId()))
+                .andExpect(jsonPath("$[1].id").value(Luna.getId()))
+                .andExpect(jsonPath("$[1].name").value(Luna.getName()))
+                .andExpect(jsonPath("$[1].age").value(Luna.getAge()))
+                .andExpect(jsonPath("$[1].gender").value(Luna.getGender()))
+                .andExpect(jsonPath("$[1].salary").value(Luna.getSalary()))
+                .andExpect(jsonPath("$[1].companyId").value(Luna.getCompanyId()));
+    }
 }
